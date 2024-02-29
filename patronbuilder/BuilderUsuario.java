ea78835aac0ba47b9b5d20708477aac217830c75
josesandovall99 <124215531+josesandovall99@users.noreply.github.com>
2024-02-29 18:46:33 -0500

    Add files via upload

---
+++ patronbuilder/BuilderUsuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package co.edu.udes.patronbuilder;
+
+/**
+ *
+ * @author JOSE SANDOVAL
+ */
+public interface BuilderUsuario {
+    public String nombres();
+    public String apellidos();
+    public String estadoCivil();
+    public String direccion();
+    public int edad();
+    public String[] tareas();
+}
---
+++ patronbuilder/PatronBuilder.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.udes.patronbuilder;
+
+/**
+ *
+ * @author JOSE SANDOVAL
+ */
+public class PatronBuilder {
+
+    public static void main(String[] args) {
+        
+//        Usuario us1 = new Usuario.Builder()
+//                .nombres("Jose Luis")
+//                .Apellidos("Sandoval Rolon")
+//                .Direccion("x")
+//                .Edad(20)
+//                .EstadoCivil("soltero")
+//                .build();
+//        
+    }
+}
